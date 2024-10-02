c357859254398e9bb19f82e08228db45674d5a69
Vignesh Acharya <vigneshh1410@gmail.com>
2024-10-02 15:22:34 +0530

    Remove unused import

--- web/src/app/api/portfolioDetails/route.ts
+++ web/src/app/api/portfolioDetails/route.ts
@@ -1,6 +1,4 @@
 import sequelize from "@/lib-db/db";
-import { PortfolioDetails } from "@/models/portfolioDetails";
-import { NextApiRequest, NextApiResponse } from "next";
 
 // Initialize the database connection and sync models if not already done
 sequelize.sync();
