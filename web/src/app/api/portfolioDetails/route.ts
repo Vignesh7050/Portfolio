578071301ebc9c002b169c5bfa1a9c2c648a9530
Vignesh Acharya <vigneshh1410@gmail.com>
2024-10-02 15:20:39 +0530

    Removed route code as its not yet implementation, and update on development process - Removing it for FE build

--- web/src/app/api/portfolioDetails/route.ts
+++ web/src/app/api/portfolioDetails/route.ts
@@ -5,34 +5,4 @@ import { NextApiRequest, NextApiResponse } from "next";
 // Initialize the database connection and sync models if not already done
 sequelize.sync();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    // Create a new portfolio
-    const { userId, title, description, url } = req.body;
-
-    try {
-      const portfolio = await PortfolioDetails.create({
-        userId,
-        title,
-        description,
-        url,
-      });
-      res.status(201).json(portfolio);
-    } catch (error) {
-      res.status(400).json({ error: error });
-    }
-  } else if (req.method === "GET") {
-    // Get all portfolios
-    try {
-      const portfolios = await PortfolioDetails.findAll();
-      res.status(200).json(portfolios);
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
-}
+export async function GET() {}
