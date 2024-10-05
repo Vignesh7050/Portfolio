8e240c72e4bd88313a754598c19e5f8cfa946f47
Vignesh Acharya <vigneshh1410@gmail.com>
2024-10-05 11:45:15 +0530

    feat: System dark and light theme UI support, profile pic viewer, roadmap viewer implementation

--- web/src/utils/profileContent.ts
+++ web/src/utils/profileContent.ts
@@ -1,61 +1,90 @@
-import { IMAGES } from "@/assets/images";
+import { IMAGES } from '@/assets/images';
 
 export const profileContent = {
-  headerTitle: "Developer",
-  authorName: "Vignesh Acharya",
-  authorDesignation: "Software Engineer",
+  headerTitle: 'Developer',
+  authorName: 'Vignesh Acharya',
+  authorDesignation: 'Software Engineer',
   profileSummary:
-    "Results-driven React.js Software Engineer with 3+ years of experience in JavaScript, TypeScript, React Js. Skilled in Agile methodologies, including sprint planning, backlog grooming, and fostering teamwork.",
-  technicalSkills: [
-    {
-      skillHeaderText: "Front End",
-      skills: [
-        { title: "JavaScript" },
-        { title: "TypeScript" },
-        { title: "React Js" },
-        { title: "Next Js" },
-        { title: "Custom hook design pattern" },
-        { title: "Redux" },
-        { title: "Context API" },
-        { title: "Html" },
-        { title: "Css" },
-        { title: "Scss" },
-      ],
-    },
-    {
-      skillHeaderText: "Version Control",
-      skills: [
-        { title: "Git" },
-        { title: "Azure repos" },
-        { title: "Bitbucket" },
-      ],
-    },
-  ],
+    'React.js Software Engineer with 3+ years of experience in JavaScript, TypeScript, and Agile methodologies. ',
   contactDetails: [
     {
       id: 1,
-      title: "Email",
-      contactUrl: "mailto:vigneshh1958@gmail.com",
+      title: 'Email',
+      contactUrl: 'mailto:vigneshh1958@gmail.com',
       icon: IMAGES.googleIcon,
     },
     {
       id: 2,
-      title: "LinkedIn",
-      contactUrl: "https://www.linkedin.com/in/vignesh-acharya-a41952190/",
+      title: 'LinkedIn',
+      contactUrl: 'https://www.linkedin.com/in/vignesh-acharya-a41952190/',
       icon: IMAGES.linkedInIcon,
     },
     {
       id: 3,
-      title: "Git Hub",
-      contactUrl: "https://github.com/Vignesh7050",
+      title: 'Git Hub',
+      contactUrl: 'https://github.com/Vignesh7050',
       icon: IMAGES.gitHubIcon,
       icon2: IMAGES.gitHubInvertedIcon,
     },
     {
       id: 4,
-      title: "LeetCode",
-      contactUrl: "https://leetcode.com/u/vigneshh1958/",
+      title: 'LeetCode',
+      contactUrl: 'https://leetcode.com/u/vigneshh1958/',
       icon: IMAGES.leetCodeIcon,
+      isHidden: true,
     },
   ],
 };
+
+export const skillsList = [
+  {
+    src: IMAGES.javaScriptIcon,
+    titleContent: 'JavaScript',
+    alt: 'javascript',
+  },
+  {
+    src: IMAGES.typeScriptIcon,
+    titleContent: 'TypeScript',
+    alt: 'typeScript',
+  },
+  {
+    src: IMAGES.reactJsIcon,
+    titleContent: 'React Js',
+    alt: 'react',
+  },
+  {
+    src: IMAGES.nextJsIcon,
+    titleContent: 'Next Js',
+    alt: 'next',
+  },
+  {
+    src: IMAGES.reduxIcon,
+    titleContent: 'Redux',
+    alt: 'redux',
+  },
+  {
+    src: IMAGES.nodeJsIcon,
+    titleContent: 'Node Js',
+    alt: 'nodejs',
+  },
+  {
+    src: IMAGES.gitIcon,
+    titleContent: 'Git',
+    alt: 'git',
+  },
+  {
+    src: IMAGES.bitBucketIcon,
+    titleContent: 'Bitbucket',
+    alt: 'bitbucket',
+  },
+  {
+    src: IMAGES.htmlIcon,
+    titleContent: 'HTML',
+    alt: 'html',
+  },
+  {
+    src: IMAGES.cssIcon,
+    titleContent: 'CSS',
+    alt: 'css',
+  },
+];
