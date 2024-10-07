d1ecb3dd26f2714a3e1ca2302e7e1af56a8d0451
Vignesh Acharya <vigneshh1410@gmail.com>
2024-10-07 08:24:48 +0530

    feat: use text constants

--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -1,9 +1,10 @@
+import { textConstants } from '@/utils';
 import { redirect } from 'next/navigation';
 
 export default function NotFound() {
   return (
     <div>
-      <h2>Redirecting to Home page, Please wait...</h2>
+      <h2>{textConstants.redirectionPageMessage}</h2>
       {redirect('/portfolio')}
     </div>
   );
